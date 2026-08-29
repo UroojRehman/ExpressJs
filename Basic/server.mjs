f8f769f73cdb8ec9e3eb8e43494527b616c55ae1
Urooj-Rehman@97 <urooj_rehman@aptechnorth.edu.pk>
2026-08-29 19:32:41 +0500

    Today Lectures

--- Basic/server.mjs
+++ Basic/server.mjs
@@ -1,11 +1,15 @@
-import express from 'express';
+import express from 'express'; 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!.. we are learning ExpressJs. Today our first Lecture....');
 });
+app.get('/about',(req,res)=>{
+  res.send("About Us")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  // console.log("Example app listening on port "+port); 
 });
