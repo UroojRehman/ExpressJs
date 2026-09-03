58cbec7047d7fd036a128397d07e4d5059d44988
Urooj-Rehman@97 <urooj_rehman@aptechnorth.edu.pk>
2026-09-03 20:37:43 +0500

    Updated

--- FileHandling/server.mjs
+++ FileHandling/server.mjs
@@ -2,12 +2,29 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+const users = [];
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 const __dirname = import.meta.dirname
 app.get('/', (req, res) => {
 //   res.send('Hello World!');
 res.sendFile(__dirname+"/index.html")
 });
+
+app.post('/', (req,res)=>{
+ const {name, email, password} = req.body
+ users.push({name, email, password});
+ let result = `
+ <h1>Submit Successfully</h1>
+ <ul>
+ ${users.map(u => `<li>${u.name} - ${u.email} - ${u.password}</li>`).join(" ")}
+ </ul>
+ `
+
+ res.send(result)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
